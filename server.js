4b8911340ddb851fc979e9dfd31d0d24c66388f3
datadiego <juandiegomariscal@gmail.com>
2024-11-04 10:46:58 +0100

    ping funciona

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express")
+const { exec } = require('child_process');
+
+//const comando = 'touch test.txt';
+//const comando = 'echo "hola mundo" > test.txt';
+//const comando = "ping -c 4 google.com"
+//const comando = "figlet hola mundo"
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
+
 app.get("/", (req,res) => {
-    res.send("Hola mundo")
+    exec(comando, (error, stdout, stderr) => {
+        res.send(`<pre>${stdout}</pre>`)
+      });
 })
 
+app.get("/ping", (req, res) => {
+    const dominio = req.query.dominio
+    const comando = "ping -c 4 " + dominio
+    exec(comando, (error, stdout, stderr) => {
+        res.send(stdout)
+    });
+})
 
 app.listen(port, () => {
     console.log(`Servidor funcionando en ${port}`)
